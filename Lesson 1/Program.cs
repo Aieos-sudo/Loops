f522c3b692c83d0c57e85d26a8f3733ba8a94124
Aieos <firered9000@yahoo.com>
2021-11-15 22:54:06 -0600

    Initial commit

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //printToConsole();
+            //Console.WriteLine("First Argument is: " + args[0]);
+            //Console.WriteLine("Second Argument is: " + args[0]);
+
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine("Argument " + i + " is " + args[i]);
+            //}
+            bool flag = false;
+            int i = 0;
+            while (flag == true)
+            {
+                Console.WriteLine("Argument " + i + " is " + args[i]);
+                ++i;
+            }
+            //int i = 0;
+            //bool flag = false;
+            //do
+            //{
+            //    Console.WriteLine("Argument " + i + " is " + args[i]);
+            //    i++;
+            //} while (flag == true);
+        }
+
+        static void printToConsole()
+        {
+            Console.WriteLine("in printToConsole()");
+
+
+
+        }
+    }
+}
